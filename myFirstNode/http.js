15b6a30f4aa1d2981124729f4eba09b5bee473a0
Nikita Agarwal <115351051+nikita-1310@users.noreply.github.com>
2023-04-09 16:43:53 +0530

    updated

--- myFirstNode/http.js
+++ myFirstNode/http.js
@@ -1,7 +1,8 @@
 var http = require('http');
+
 http.createServer((req,res)=>{
 res.write("HTTP server is created");
 res.end();
 }).listen(5050,()=>{
-    console.log("HTTP Server is started")
+    console.log("HTTP Server is running")
 })
